96b1e7a986785b7499a7959ea4298beda108f0bc
Nworah William <yongliamm@gmail.com>
2022-03-28 12:17:54 +0530

    nonce

--- src/components/Blockchain/Miner/CandidateBlock.js
+++ src/components/Blockchain/Miner/CandidateBlock.js
@@ -1,10 +1,18 @@
-
 import { useEffect, useState } from 'react';
 import { GiMiner } from 'react-icons/gi'
+import { IoMdCube } from 'react-icons/io'
 import { colors } from '../../Others/Colors';
+const SHA256 = require("crypto-js/sha256");
 
-//UTXO[tid][blockHash]
+const difficulty = 4;
 const blockTX = [
+    {
+        hash: 'tx-gsfdh566bes5ggmmhgjoh484hehe4rg',
+        fee: 0.39,
+        reward: 6.39,
+        timestamp: + new Date(),
+        to: 'sc-dffvfog05954jnknb9hthm9je3ngn9ijslmkfzgreg9'
+    },
     {
         hash: 'tx-dkhfkhskfhskggmmhgjoh484hehe4rg',
         amount: 0,
@@ -23,81 +31,175 @@ function CandidateBlock() {
     const [ipUTXO, setIpUTXO] = useState([]);
     const [blockHash, setBlockHash] = useState('');
     const [blockIsValid, setBlockIsValid] = useState(false);
+    const [autoMining, setAutoMining] = useState(false);
+    const [autoMiningStart, setAutoMiningStart] = useState(0);
+    const [autoMiningEnd, setAutoMiningEnd] = useState(0);
     const [nonce, setNonce] = useState(0);
+    const [tempNoncee, settempNoncee] = useState(0);
+    const [block, setBlock] = useState({
+        hash: findHash("0eb6638c4f44e941942a5d8dd51fc92e1bacec92a78a154522060fdbeb2daf0e", + new Date(), 'tx-tl79jg5hk8reh3hdfjd90wpo5gd83s', 0),
+        timestamp: + new Date(),
+        data: 'tx-tl79jg5hk8reh3hdfjd90wpo5gd83s',
+        nonce: 0,
+        prevHash: "0eb6638c4f44e941942a5d8dd51fc92e1bacec92a78a154522060fdbeb2daf0e"
+    });
     const [ipUTXOamount, setIpUTXOamount] = useState(0);
     const [showSubmitButton, setShowSubmitButton] = useState(false);
+    const [createBlock, setCreateBlock] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [blockTime, setBlockTime] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => setBlockTime(Math.round((+ new Date() - block.timestamp) / 1000)), 1000)
+    }, [blockTime])
 
     useEffect(() => {
         setBlockIsValid(false)
         setShowSubmitButton(false)
-        setBlockHash('')
-        calculateHash();
-    }, [nonce, blockTX])
-    function calculateHash() {
-        setTimeout(() => {
-            setBlockHash('00hgbnfmdhdgll')
-            setShowSubmitButton(true)
+        if (!autoMining) {
+            setBlock(block => ({ ...block, hash: findHash(block.prevHash, block.timestamp, block.data, nonce), nonce: nonce }))
+        }
+        else {
             setBlockIsValid(true)
-        }, 3000)
+            setShowSubmitButton(true)
+            setAutoMining(false)
+        }
+    }, [nonce, blockTX])
+
+    useEffect(() => {
+        if (autoMining) {
+            let tempNonce = nonce;
+            let hash = block.hash;
+            if (!checkDifficulty(hash, difficulty)) {
+                setAutoMiningStart(+ new Date())
+                setShowSubmitButton(false)
+                setBlockIsValid(false)
+                while (!checkDifficulty(hash, difficulty)) {
+                    tempNonce++
+                    hash = findHash(block.prevHash, block.timestamp, block.data, tempNonce)
+                    // affects time
+                    // console.log(tempNonce)
+                }
+                setBlock(block => ({ ...block, hash: hash, nonce: tempNonce }));
+                setAutoMiningEnd(+ new Date())
+            } else
+                setAutoMining(false)
+        }
+    }, [autoMining])
+
+    useEffect(() => {
+        console.log(block)
+        if (block.hash !== undefined) {
+            if (autoMining) {
+                console.log('Mining Time: ', autoMiningEnd - autoMiningStart);
+                setShowSubmitButton(true)
+                setBlockIsValid(true)
+                setNonce(block.nonce)
+            } else {
+                let isBlockValid = checkDifficulty(block.hash, difficulty);
+                setShowSubmitButton(isBlockValid)
+                setBlockIsValid(isBlockValid)
+            }
+        }
+    }, [block])
+
+    function findHash(previousHash, timestamp, data, nonce) {
+        return SHA256(previousHash + timestamp + data + nonce).toString();
+    }
+    function checkDifficulty(hash, difficulty) {
+        return hash.substr(0, difficulty) === "0".repeat(difficulty)
     }
 
-    function CandidateBlock(e) {
+    function BroadcastBlock(e) {
         e.preventDefault();
         setLoading(true)
     }
 
     return (
-        <form onSubmit={CandidateBlock} className='container' style={{
-            width: '100%',
-            backgroundColor: blockIsValid ? '' : '#6c3c3c'
-        }}>
-            <h4>Block #14</h4>
-            <div className='form-field'>
-                <table style={{ fontSize: 16 }}>
-                    <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
-                        <tr><td>Prev. Hash</td> <td>0000ghgfjdhgfyh76743h23-47ujgjdjfhg</td></tr>
-                        <tr><td>Difficulty</td> <td>27</td></tr>
-                        <tr><td>Merkle Root</td> <td>jkjkgf-mfjhsdk48kg78vkf740-v</td></tr>
-                        <tr><td>Hash (POW)</td> <td>{blockHash}</td></tr>
-                    </div>
-                </table>
-            </div>
-            <div className='form-field'>
-                <label>Transactions</label>
-                <table style={{ fontSize: 16 }}>
-                    <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
-                        <tr><td>Block Reward</td> <td>6.39 SC</td></tr>
-                        <tr><td>Fee Reward</td> <td>0.39 SC</td></tr>
-                    </div>
-                    {blockTX.map((transaction, index) => (
-                        <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
-                            <tr><td>Hash</td> <td>{transaction.hash}</td></tr>
-                            <tr><td>Fee</td> <td>{transaction.amount} SC</td></tr>
+        <>
+            {createBlock ?
+                <button onClick={() => {
+                    setCreateBlock(true)
+                    setBlock(block => ({ ...block, timestamp: + new Date() }));
+                }}>Create Block</button>
+                :
+                <form onSubmit={BroadcastBlock} className='container' style={{
+                    width: '100%',
+                    backgroundColor: blockIsValid ? '' : '#6c3c3c'
+                }}>
+                    <div style={{ width: '100%', display: 'flex' }}>
+                        <h4 style={{ textAlign: 'left', flex: 1 }}>Block #14</h4>
+                        <div style={{ display: 'inline-grid', justifyContent: 'center', alignContent: 'center' }}>
+                            <h4 style={{ textAlign: 'right', fontSize: 20, margin: 0 }}>
+                                <GiMiner color='#e5f9ff' /> Mining Duration: {((autoMiningEnd - autoMiningStart) / 1000).toFixed(1)}s</h4>
+                            <h4 style={{ textAlign: 'right', fontSize: 20, margin: 0 }}>
+                                <IoMdCube color={colors.link} /> Total Block time: {blockTime}s</h4>
                         </div>
-                    ))
-                    }
-                </table>
-            </div>
-            <div className='form-field'>
-                <label>Nonce</label>
-                <input type={'number'} value={nonce}
-                    onChange={(e) => setNonce(+e.target.value)} required readOnly={loading} />
-            </div>
-            <div className='btn-div'>
-                {showSubmitButton ?
-                    loading ?
-                        <div className='loader'></div>
-                        : <button disabled={!blockIsValid}
-                            style={{ background: blockIsValid ? colors.lighter : '' }}>Broadcast</button>
-                    :
-                    <center><div className='loader'></div>
-                        <div style={{ fontStyle: 'italic', fontSize: 18 }}>Solving hash...</div></center>}
-            </div>
-            <div className='btn-div'>
-                <GiMiner color='#e5f9ff' size={40} />
-            </div>
-        </form>
+                    </div>
+
+                    <div className='form-field'>
+                        <table style={{ fontSize: 16 }}>
+                            <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
+                                <tr><td>Prev. Hash</td> <td>{block.prevHash}</td></tr>
+                                <tr><td>Difficulty</td> <td>{difficulty}</td></tr>
+                                <tr><td>Merkle Root</td> <td>{block.data}</td></tr>
+                                <tr><td>Hash (POW)</td> <td>{autoMining ?
+                                    <div className='loader'
+                                        style={{ width: 20, height: 20 }}></div>
+                                    :
+                                    block.hash}</td></tr>
+                            </div>
+                        </table>
+                    </div>
+                    <div className='form-field'>
+                        <label>Transactions</label>
+                        <table style={{ fontSize: 16 }}>
+                            <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
+                                <tr><td>Block Reward</td> <td>{blockTX[0].reward} SC</td></tr>
+                                <tr><td>Fee Reward</td> <td>{blockTX[0].fee} SC</td></tr>
+                            </div>
+                            {blockTX.map((transaction, index) => (
+                                index !== 0 ?
+                                    <div style={{ textAlign: 'left', background: '#6ba9a8', marginBottom: 5, padding: 10 }}>
+                                        <tr><td>Hash</td> <td>{transaction.hash}</td></tr>
+                                        <tr><td>Fee</td> <td>{transaction.amount} SC</td></tr>
+                                    </div>
+                                    :
+                                    null
+                            ))
+                            }
+                        </table>
+                    </div>
+                    <div className='form-field'>
+                        <label>Nonce</label>
+                        <input type={'number'} value={nonce}
+                            onChange={(e) => setNonce(+e.target.value)} required readOnly={loading} />
+                    </div>
+                    <div className='btn-div'>
+                        {showSubmitButton ?
+                            loading ?
+                                <div className='loader'></div>
+                                : <button disabled={!blockIsValid}
+                                    style={{ background: blockIsValid ? colors.lighter : '' }}>Broadcast</button>
+                            :
+                            <center>
+                                <div style={{ fontStyle: 'italic', fontSize: 18 }}>
+                                    {autoMining ? 'Mining...' : 'Block invalid'}</div></center>}
+                    </div>
+                    <div className='btn-div'>
+                        {autoMining ?
+                            <center><div className='loader'></div>
+                            </center>
+                            :
+                            <GiMiner
+                                onClick={() => {
+                                    setAutoMining(true)
+                                }}
+                                color='#e5f9ff' size={40} />}
+                    </div>
+                </form>
+            }
+        </>
     )
 }
 export default CandidateBlock
